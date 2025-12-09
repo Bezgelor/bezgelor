b8f6fea44a89d0a977ebc5e9318e9e8dcc736e68
jrimmer <jason@rimmer.net>
2025-12-09 12:10:03 -0800

    chore: Add bezgelor_crypto app skeleton

---
+++ apps/bezgelor_crypto/lib/bezgelor_crypto.ex
@@ -0,0 +1,18 @@
+defmodule BezgelorCrypto do
+  @moduledoc """
+  Documentation for `BezgelorCrypto`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BezgelorCrypto.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/bezgelor_crypto/test/bezgelor_crypto_test.exs
@@ -0,0 +1,8 @@
+defmodule BezgelorCryptoTest do
+  use ExUnit.Case
+  doctest BezgelorCrypto
+
+  test "greets the world" do
+    assert BezgelorCrypto.hello() == :world
+  end
+end
