a6831d9a5a15f6d8e596c34a6730596973bdd047
jrimmer <jason@rimmer.net>
2025-12-10 10:05:43 -0800

    feat(db): Add Mounts context with customization

---
+++ apps/bezgelor_db/lib/bezgelor_db/mounts.ex
@@ -0,0 +1,59 @@
+defmodule BezgelorDb.Mounts do
+  @moduledoc """
+  Active mount management with customization.
+  """
+
+  alias BezgelorDb.Repo
+  alias BezgelorDb.Schema.ActiveMount
+  alias BezgelorDb.Collections
+
+  @spec get_active_mount(integer()) :: ActiveMount.t() | nil
+  def get_active_mount(character_id) do
+    Repo.get_by(ActiveMount, character_id: character_id)
+  end
+
+  @spec set_active_mount(integer(), integer(), integer()) ::
+          {:ok, ActiveMount.t()} | {:error, :not_owned | term()}
+  def set_active_mount(character_id, account_id, mount_id) do
+    if Collections.owns_mount?(account_id, character_id, mount_id) do
+      case get_active_mount(character_id) do
+        nil ->
+          %ActiveMount{}
+          |> ActiveMount.changeset(%{character_id: character_id, mount_id: mount_id})
+          |> Repo.insert()
+
+        existing ->
+          existing
+          |> ActiveMount.changeset(%{mount_id: mount_id, customization: %{}})
+          |> Repo.update()
+      end
+    else
+      {:error, :not_owned}
+    end
+  end
+
+  @spec clear_active_mount(integer()) :: :ok
+  def clear_active_mount(character_id) do
+    case get_active_mount(character_id) do
+      nil -> :ok
+      mount ->
+        Repo.delete(mount)
+        :ok
+    end
+  end
+
+  @spec update_customization(integer(), map()) ::
+          {:ok, ActiveMount.t()} | {:error, :no_active_mount | term()}
+  def update_customization(character_id, customization) do
+    case get_active_mount(character_id) do
+      nil ->
+        {:error, :no_active_mount}
+
+      mount ->
+        new_customization = Map.merge(mount.customization, customization)
+        mount
+        |> ActiveMount.customization_changeset(new_customization)
+        |> Repo.update()
+    end
+  end
+end
---
+++ apps/bezgelor_db/test/mounts_test.exs
@@ -0,0 +1,66 @@
+defmodule BezgelorDb.MountsTest do
+  use ExUnit.Case
+
+  alias BezgelorDb.{Accounts, Characters, Collections, Mounts, Repo}
+
+  @moduletag :database
+
+  setup do
+    case Repo.start_link([]) do
+      {:ok, _pid} -> :ok
+      {:error, {:already_started, _pid}} -> :ok
+    end
+
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+    Ecto.Adapters.SQL.Sandbox.mode(Repo, {:shared, self()})
+
+    email = "mount_test#{System.unique_integer([:positive])}@test.com"
+    {:ok, account} = Accounts.create_account(email, "password123")
+
+    {:ok, character} =
+      Characters.create_character(account.id, %{
+        name: "Rider#{System.unique_integer([:positive])}",
+        sex: 0, race: 0, class: 0, faction_id: 166, world_id: 1, world_zone_id: 1
+      })
+
+    # Unlock a mount for tests
+    {:ok, _} = Collections.unlock_account_mount(account.id, 1001, "purchase")
+
+    {:ok, account: account, character: character}
+  end
+
+  describe "active mount" do
+    test "set_active_mount activates mount", %{account: account, character: character} do
+      {:ok, mount} = Mounts.set_active_mount(character.id, account.id, 1001)
+      assert mount.mount_id == 1001
+    end
+
+    test "get_active_mount returns current mount", %{account: account, character: character} do
+      {:ok, _} = Mounts.set_active_mount(character.id, account.id, 1001)
+      mount = Mounts.get_active_mount(character.id)
+      assert mount.mount_id == 1001
+    end
+
+    test "set_active_mount fails if not owned", %{account: account, character: character} do
+      {:error, :not_owned} = Mounts.set_active_mount(character.id, account.id, 9999)
+    end
+
+    test "clear_active_mount removes mount", %{account: account, character: character} do
+      {:ok, _} = Mounts.set_active_mount(character.id, account.id, 1001)
+      :ok = Mounts.clear_active_mount(character.id)
+      assert Mounts.get_active_mount(character.id) == nil
+    end
+  end
+
+  describe "customization" do
+    test "update_customization changes mount look", %{account: account, character: character} do
+      {:ok, _} = Mounts.set_active_mount(character.id, account.id, 1001)
+      {:ok, mount} = Mounts.update_customization(character.id, %{
+        "dyes" => [1, 2, 3],
+        "flair" => ["flag_01"]
+      })
+
+      assert mount.customization["dyes"] == [1, 2, 3]
+    end
+  end
+end
